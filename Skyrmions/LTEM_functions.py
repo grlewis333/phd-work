81360de8a4daabac55603b3952862e65ee4e807c
George Lewis <grlewis333@hotmail.co.uk>
2021-02-15 16:46:43 +0000

    set up bloch simulations

--- Skyrmions/LTEM_functions.py
+++ Skyrmions/LTEM_functions.py
@@ -1,17 +1,20 @@
 import numpy as np                              # For maths
 from scipy import constants                     # For scientific constants
 from skimage.restoration import unwrap_phase    # unwrap phase
+from matplotlib import pyplot as plt
+from scipy import ndimage                       # For image rotations
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-def B_phase_calc(MXr,MYr,MZr,ds = 0.1,kx = 0.1,ky = 0.1,kv = 300,Cs = 8000,Msat = 480767.832897):
+def B_phase_calc(MXr,MYr,MZr,ds = 0.1,kx = 0.1,ky = 0.1,kv = 300,Cs = 8000,Msat = 480767.832897,x_res =1,y_res=1,z_res=1):
 
     #ds # Defocus step in mm
     #kx # Tikhonov filter radius in x in pixels
     #ky # Tikhonov filter radius in y in pixels
     #kv # Acceleratig voltage of electrons in kV
     #Cs # Spherical aberration coefficient in mm
-    with HiddenPrints():
-        x_res, y_res, z_res, x_size, y_size, z_size, x_begin, y_begin, z_begin, x_end, y_end, z_end = unify_input(X,Y,Z,MXr,MYr,MZr)
-    
+#     with HiddenPrints():
+#         x_res, y_res, z_res, x_size, y_size, z_size, x_begin, y_begin, z_begin, x_end, y_end, z_end = unify_input(X,Y,Z,MXr,MYr,MZr)
+    x_size,y_size,z_size = np.shape(MXr)
     
     MXpad = np.pad(MXr,[(100,100),(100,100),(100,100)], mode='constant', constant_values=0)
     MYpad = np.pad(MYr,[(100,100),(100,100),(100,100)], mode='constant', constant_values=0)
@@ -57,3 +60,261 @@ def calculate_B(phase_B):
     mag_B = np.hypot(b_field_x,b_field_y)
     
     return mag_B,b_field_x,b_field_y
+
+def plot_2d(X,Y,Z,MX,MY,MZ,s=5,size=0.1, width = 0.005, title=''):
+    """
+    Plot magnetisation data in 2D
+    
+    Input:
+    x,y = 'Projected' 2D coordinates (nxn)
+    u,v = 'Projected' 2D magnetisation (nxn)
+    s = Quiver plot skip density
+    size = Arrow length scaling
+    width = Arrow thickness 
+    
+    Output:
+    2D Plot of magnetisation:
+    - Arrows show direction of M
+    - Background color shows magnitude of M
+    """
+    # Project along z by averaging
+    x_proj = np.mean(X,axis=2)
+    y_proj = np.mean(Y,axis=2)
+    z_proj = np.mean(Z,axis=2)
+    u_proj = np.mean(MX,axis=2)
+    v_proj = np.mean(MY,axis=2)
+    w_proj = np.mean(MZ,axis=2)
+    
+    # Create figure
+    fig = plt.figure(figsize=(6, 8))
+    grid = plt.GridSpec(4, 3)
+    ax1 = fig.add_subplot(grid[3, 0])
+    ax2 = fig.add_subplot(grid[3, 1])
+    ax3 = fig.add_subplot(grid[3, 2])
+    ax4 = fig.add_subplot(grid[:3, :])
+    
+    # plot Mx
+    pos =  ax1.imshow(np.flipud(u_proj.T), vmin=-1,vmax=1,cmap='RdBu'); 
+    ax1.set_title('Mx',fontsize=13); ax1.set_xlabel('x',fontsize=14);ax1.set_ylabel('y',fontsize=14);
+
+    # Plot My
+    pos = ax2.imshow(np.flipud(v_proj.T), vmin=-1,vmax=1,cmap='RdBu');
+    ax2.set_title('My',fontsize=13); ax2.set_xlabel('x',fontsize=14);ax2.set_ylabel('y',fontsize=14); 
+
+    #Plot Mz
+    pos = ax3.imshow(np.flipud(w_proj.T), vmin=-1,vmax=1,cmap='RdBu',extent=[x_begin, x_end, y_begin, y_end]);
+    ax3.set_title('Mz',fontsize=13); ax3.set_xlabel('x',fontsize=14);ax3.set_ylabel('y',fontsize=14); 
+    fig.colorbar(pos,ax=ax3,fraction=0.046, pad=0.04)
+
+    # Main arrow plot
+    ax4.quiver(x_proj[::s,::s],y_proj[::s,::s],u_proj[::s,::s],v_proj[::s,::s],scale=1/size,pivot='mid',width=width)
+    
+    # Calculate vector magnitude
+    magnitude = (u_proj**2 + v_proj**2)**0.5
+    
+    # Plot vector magnitude
+    im1 = ax4.imshow(np.flipud(magnitude.T),vmin=0,vmax=1,cmap='Blues',
+                     extent=(np.min(x_proj),np.max(x_proj),np.min(y_proj),np.max(y_proj)))
+    
+    # Add colorbar and labels
+    clb = fig.colorbar(im1,ax=ax4,fraction=0.046, pad=0.04)
+    ax4.set_xlabel('x / nm',fontsize=14)
+    ax4.set_ylabel('y / nm',fontsize=14)
+    ax4.set_title(title, fontsize= 16)
+    for ax in [ax1,ax2,ax3]:
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+    
+    plt.tight_layout()
+    
+def rotate_bulk(U,V,W,ax,ay,az):
+    """ 
+    Rotate magnetisation locations from rotation angles ax,ay,az 
+    about the x,y,z axes (given in degrees) 
+    
+    NOTE: This implementation of scipy rotations is EXTRINSIC
+    Therefore, to make it compatible with our intrinsic vector
+    rotation, we swap the order of rotations (i.e. x then y then z)
+    """
+    # Due to indexing, ay needs reversing for desired behaviour
+    ay = -ay
+    
+    U = ndimage.rotate(U,ax,reshape=False,axes=(1,2),order=1)
+    U = ndimage.rotate(U,ay,reshape=False,axes=(2,0),order=1)
+    U = ndimage.rotate(U,az,reshape=False,axes=(0,1),order=1)
+    
+    V = ndimage.rotate(V,ax,reshape=False,axes=(1,2),order=1)
+    V = ndimage.rotate(V,ay,reshape=False,axes=(2,0),order=1)
+    V = ndimage.rotate(V,az,reshape=False,axes=(0,1),order=1)
+    
+    W = ndimage.rotate(W,ax,reshape=False,axes=(1,2),order=1)
+    W = ndimage.rotate(W,ay,reshape=False,axes=(2,0),order=1)
+    W = ndimage.rotate(W,az,reshape=False,axes=(0,1),order=1)
+
+    return U,V,W
+
+def grid_to_coor(U,V,W):
+    """ Convert gridded 3D data (3,n,n,n) into coordinates (n^3, 3) """
+    coor_flat = []
+    nx,ny,nz = np.shape(U)
+    for ix in range(nx):
+        for iy in range(ny):
+            for iz in range(nz):
+                x = U[ix,iy,iz]
+                y = V[ix,iy,iz]
+                z = W[ix,iy,iz]
+                coor_flat.append([x,y,z])
+                
+    return coor_flat
+
+def coor_to_grid(coor_flat,shape):
+    """ Convert coordinates (n^3, 3) into gridded 3D data (3,n,n,n) """
+    n = int(np.round(np.shape(coor_flat)[0]**(1/3)))
+    nx,ny,nz = shape
+    x = np.take(coor_flat,0,axis=1)
+    y = np.take(coor_flat,1,axis=1)
+    z = np.take(coor_flat,2,axis=1)
+    U = x.reshape((nx,ny,nz))
+    V = y.reshape((nx,ny,nz))
+    W = z.reshape((nx,ny,nz))
+
+    return U, V, W
+
+def rotation_matrix(ax,ay,az):
+    """ 
+    Generate 3D rotation matrix from rotation angles ax,ay,az 
+    about the x,y,z axes (given in degrees) 
+    (Uses convention of rotating about z, then y, then x)
+    """
+
+    ax = ax * np.pi/180
+    Cx = np.cos(ax)
+    Sx = np.sin(ax)
+    mrotx = np.array([[1,0,0],[0,Cx,-Sx],[0,Sx,Cx]])
+    
+    ay = ay * np.pi/180
+    Cy = np.cos(ay)
+    Sy = np.sin(ay)
+    mroty = np.array([[Cy,0,Sy],[0,1,0],[-Sy,0,Cy]])
+    
+    az = az * np.pi/180
+    Cz = np.cos(az)
+    Sz = np.sin(az)
+    mrotz = np.array([[Cz,-Sz,0],[Sz,Cz,0],[0,0,1]])
+    
+    mrot = mrotz.dot(mroty).dot(mrotx)
+    
+    return mrot
+
+def rotate_vector(coor_flat,ax,ay,az):
+    """ Rotates vectors by specified angles ax,ay,az 
+    about the x,y,z axes (given in degrees) """
+    
+    # Get rotation matrix
+    mrot = rotation_matrix(ax,ay,az)    
+
+    coor_flat_r = np.zeros_like(coor_flat)
+    
+    # Apply rotation matrix to each M vector
+    for i,M in enumerate(coor_flat):
+        coor_flat_r[i] = mrot.dot(M)
+    
+    return coor_flat_r
+
+def rotate_magnetisation(U,V,W,ax=0,ay=0,az=0):
+    """ 
+    Takes 3D gridded magnetisation values as input
+    and returns them after an intrinsic rotation ax,ay,az 
+    about the x,y,z axes (given in degrees) 
+    (Uses convention of rotating about z, then y, then x)
+    """
+    # Rotate the gridded locations of M values
+    Ub, Vb, Wb = rotate_bulk(U,V,W,ax,ay,az)
+    
+    # Convert gridded values to vectors
+    coor_flat = grid_to_coor(Ub,Vb,Wb)
+    
+    # Rotate vectors
+    coor_flat_r = rotate_vector(coor_flat,ax,ay,az)
+    
+    # Convert vectors back to gridded values
+    shape = np.shape(U)
+    Ur,Vr,Wr = coor_to_grid(coor_flat_r,shape)
+    
+    # Set small values to 0
+    # (In theory the magnitude of M in each cell should be Ms,
+    #  so we can set magnitude lower than this to zero -
+    #  typically python rounding errors lead to very small values,
+    #  which it is useful to exclude here)
+#     mag_max = (np.max(U)**2+np.max(V)**2+np.max(W)**2)**0.5
+#     mag = (Ur**2+Vr**2+Wr**2)**.5
+#     for M in [Ur,Vr,Wr]:
+#         M[abs(M)<1e-5*mag_max] = 0
+#         M[mag<.6*mag_max] = 0
+    
+    return Ur,Vr,Wr
+
+def plot_phase(Mx,My,Mz,ax=None):
+    if ax == None:
+        plt.figure(figsize=(9,9))
+        ax = plt.gca()
+        
+    # Get phase image
+    phase_B = B_phase_calc(Mx,My,Mz,x_res=3,y_res=3,z_res=2)
+
+    # Unwrap phase image
+    pu = unwrap_phase(phase_B)
+
+    # plot
+    ax.imshow(pu[:,::-1].T,cmap='Greys')
+    ax.axis('off')
+    ax.set_title('Phase image',fontsize=25)
+    
+def omf_to_mag(data):
+    """ Extract magnetization in grid array from ubermag 'system' object """
+    #ms = system.m.array
+    ms = data
+    shape = np.shape(ms)
+
+    xs,ys,zs,mx,my,mz = [],[],[],[],[],[]
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            for k in range(shape[2]):
+                xs.append(i)
+                ys.append(j)
+                zs.append(k)
+                mx.append(ms[i][j][k][0])
+                my.append(ms[i][j][k][1])
+                mz.append(ms[i][j][k][2])
+    Mx,My,Mz = np.reshape(mx,(shape[0],shape[1],shape[2])),\
+                            np.reshape(my,(shape[0],shape[1],shape[2])), \
+                            np.reshape(mz,(shape[0],shape[1],shape[2]))
+    return Mx,My,Mz
+
+def plot_mag(Mx,My,Mz,direction = 'z',s=1,ax=None):
+    if ax == None:
+        plt.figure(figsize=(9,9))
+        ax = plt.gca()
+    
+    if direction == 'z':
+        M_proj = np.mean(Mz,axis=2)[:,::-1].T
+        a = np.mean(Mx,axis=2)[:,::-1].T
+        b = np.mean(My,axis=2)[:,::-1].T
+        
+    if direction == 'y':
+        M_proj = np.mean(My,axis=1)[:,::-1].T
+        a = np.mean(Mx,axis=1)[:,::-1].T
+        b = np.mean(Mz,axis=1)[:,::-1].T
+        
+    ax.quiver(a[::s,::s],b[::s,::s],pivot='middle',width=0.006,scale=8e6,edgecolors='w',linewidth=.5)
+    mmax = np.max((Mx**2+My**2+Mz**2)**0.5)
+    im = ax.imshow(M_proj,cmap='seismic',extent=(0-.5,np.shape(a[::s,::s])[1]-.5,np.shape(a[::s,::s])[0]-.5,0-.5),vmin=-mmax,vmax=mmax)
+    ax.axis('off')
+    ax.set_title('Projected Magnetisation',fontsize=25)
+    
+    
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+
+    cbar = plt.colorbar(im,cax=cax)
+    cbar.ax.set_ylabel('M%s' %direction ,rotation=0,fontsize=20)
